f1ad087cd9ae1a8a2379c9b6b1b4817ee45f2998
Abdullah Toumeh <abdoullah.tomeh@gmail.com>
2018-07-12 11:35:06 -0700

    adding helper.js

---
+++ api/helper.js
@@ -0,0 +1,9 @@
+const { data } = require("./data");
+const helpers = {
+  // An example to get you started...
+  getMovies() {
+    return data.movies;
+  }
+  // All of your other helpers will go here...
+};
+module.exports = { helpers };

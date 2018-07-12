ab5554cb93c8f5604af793a3b7635bd722ebbac8
Abdullah Toumeh <abdoullah.tomeh@gmail.com>
2018-07-12 10:19:11 -0700

    adding query and Movie to resolver.js

--- api/resolvers.js
+++ api/resolvers.js
@@ -1,5 +1,27 @@
 const data = require("./data");
 const resolvers = {
-  // Resolvers go here...
+  Query: {
+    movies() {
+      return data.movies;
+    },
+    movie(root, { id }) {
+      return data.movies.find(movie => movie.id === parseInt(id));
+    }
+  },
+
+  Movie: {
+    director(movie) {
+      if (!movie.director) return null;
+      return data.people.find(person => person.id === movie.director);
+    },
+    stars(movie) {
+      return data.people.filter(person => (
+        person.filmography.find(credit => (
+          credit === movie.id && person.id !== movie.director
+        ))
+      ));
+    },
+  },
+
 };
 module.exports = resolvers

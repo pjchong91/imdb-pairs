eb6ebd7774293320d4c2759f1b3e36e2d5880886
Abdullah Toumeh <abdoullah.tomeh@gmail.com>
2018-07-12 10:11:37 -0700

    adding query Person and Movie to schema

--- api/schema.js
+++ api/schema.js
@@ -1,6 +1,24 @@
 const { gql } = require("apollo-server-express");
 module.exports = gql`
-  # Add the Person type here...
-  # Add the Movie type here...
-  # Add the root Query type here...
+  type Query {
+    people: [Person]
+    person(id: ID!): Person
+    movies: [Movie]
+    movie(id: ID!): Movie
+  }  
+  type Person {
+    id: ID!
+    name: String!
+    birthday: String
+    placeOfBirth: String
+    bio: String
+    filmography: [Movie]
+  }
+  type Movie {
+    id: ID!
+    title: String!
+    releaseDate: String
+    stars: [Person]
+    director: Person
+  }
 `;
